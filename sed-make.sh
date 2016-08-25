0879e09502b5c122033cc4cbf6b0f13ace6360b3
John Bickar <jbickar@stanford.edu>
2016-08-25 14:42:20 -0700

    Update helper comment for sed-make.sh

--- sed-make.sh
+++ sed-make.sh
@@ -2,6 +2,7 @@
 
 # Usage: ./sed-make <module> <oldversion> <newversion>
 # E.g., "./sed-make block_class 1.3 2.3"
+# E.g., "./sed-make.sh stanford_events_importer 7.x-3.2 7.x-3.3
 
 if [[ $1 == *"stanford"* ]]; then
   find . -name "*.make" -exec sed -i '' 's/'$1'\]\[download\]\[tag\] \= \"'$2'\"/'$1'\]\[download\]\[tag\] \= \"'$3'\"/g' "{}" \;
