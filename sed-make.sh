e9dd9d36fa598dc6f2ad46f0e07e2372740a1c8a
John Bickar <jbickar@stanford.edu>
2016-08-25 14:36:02 -0700

    Update sed-make.sh to work with stanford modules/themes cloned from github

--- sed-make.sh
+++ sed-make.sh
@@ -3,5 +3,9 @@
 # Usage: ./sed-make <module> <oldversion> <newversion>
 # E.g., "./sed-make block_class 1.3 2.3"
 
+if [[ $1 == *"stanford"* ]]; then
+  find . -name "*.make" -exec sed -i '' 's/'$1'\]\[download\]\[tag\] \= \"'$2'\"/'$1'\]\[download\]\[tag\] \= \"'$3'\"/g' "{}" \;
+fi
+
 find . -name "*.make" -exec sed -i '' 's/'$1'\]\[version\] \= \"'$2'\"/'$1'\]\[version\] \= \"'$3'\"/g' "{}" \;
 
