16d5ab9245fb52b3831ec7500a78af8a3e41a77d
John Bickar <jbickar@stanford.edu>
2016-08-25 14:48:33 -0700

    Update sed-make.sh to work with open_framework

--- sed-make.sh
+++ sed-make.sh
@@ -7,6 +7,9 @@
 if [[ $1 == *"stanford"* ]]; then
   find . -name "*.make" -exec sed -i '' 's/'$1'\]\[download\]\[tag\] \= \"'$2'\"/'$1'\]\[download\]\[tag\] \= \"'$3'\"/g' "{}" \;
 fi
+if [[ $1 == *"open_framework"* ]]; then
+  find . -name "*.make" -exec sed -i '' 's/'$1'\]\[download\]\[tag\] \= \"'$2'\"/'$1'\]\[download\]\[tag\] \= \"'$3'\"/g' "{}" \;
+fi
 
 find . -name "*.make" -exec sed -i '' 's/'$1'\]\[version\] \= \"'$2'\"/'$1'\]\[version\] \= \"'$3'\"/g' "{}" \;
 
